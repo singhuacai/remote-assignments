d8c9e2cdd9c866632f167528c574346d3c1f7fb2
singhuacai <singhuacai@gmail.com>
2021-08-05 11:51:43 +0800

    Add th function of "Click to Show More Content Boxes."

--- Week-2/Assignment-4/JS_add_DOM.js
+++ Week-2/Assignment-4/JS_add_DOM.js
@@ -1,5 +1,10 @@
 const message = document.querySelector('h1.message');
-
+const button = document.querySelector('.button');
+const hideBoxDiv = document.querySelector('.hideBox');
 message.addEventListener('click', () => {
     message.innerHTML = "Have a Good Time!";
+});
+
+button.addEventListener('click', () => {
+    hideBoxDiv.style.display = 'block';
 });
