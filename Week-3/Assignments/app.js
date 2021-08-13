54c0a98112c606c27f4a6a08d2e7d2815e56340f
singhuacai <singhuacai@gmail.com>
2021-08-13 21:25:10 +0800

    add the http cookie file

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
+var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
     res.send("<h1> Hello, My Server! </h1>");
 })
-
+/*==============Assignment2 & 3 SUM(below)=================*/
 app.get('/data', (req, res) => {
     const param = req.query.number;           // param = req.query.number =>string
     if(param){
@@ -18,10 +24,26 @@ app.get('/data', (req, res) => {
         }
     }
     return res.send('<h1>Lack of Parameter</h1>');
+});
+
+/*==============Assignment4 Cookie(below)=================*/
+app.get('/myName', (req, res) => {
+    const name = req.cookies.name;
+    if(name){
+        res.render('index',{ name });
+    }else{
+        res.render('input');
+    }
 })
 
-app.get('/myName', (req, res) => {
-    res.send("<h2> Welcome to my Server! </h2>");
+app.post('/trackName', (req, res) =>{
+    res.cookie('name', req.body.name);
+    res.redirect('/myName');
+})
+
+app.post('/goodbye',(req, res) => {
+    res.clearCookie('name');
+    res.redirect('/myName');
 })
 
 app.listen('3000',() =>{
