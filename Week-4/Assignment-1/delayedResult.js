4ceb0a43af2531c3b25bd7c7fa257f00bf167dee
singhuacai <singhuacai@gmail.com>
2021-08-16 17:28:06 +0800

    add the delayedResult function

---
+++ Week-4/Assignment-1/delayedResult.js
@@ -0,0 +1,12 @@
+function delayedResult(n1, n2, delayTime, callback) {
+    callback = setTimeout(() => {console.log(`${n1+n2}`)}, delayTime);
+    // callback = setTimeout(() => {return(n1+n2)}, delayTime);
+};
+
+// delayedResult(4, 5, 3000, function(result) {
+//     console.log(result);
+// }); // 9 (4+5) will be shown in the console after 3 seconds
+
+delayedResult(-5, 10, 2000, function(result) {
+    console.log(result);
+}); // 5 (-5+10) will be shown in the console after 2 seconds
